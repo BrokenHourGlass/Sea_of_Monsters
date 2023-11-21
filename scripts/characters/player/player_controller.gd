02e75943b98ab7b19240fccc0963f341dee1dbf0
BrokenHourGlass <jacobdigby@protonmail.com>
2023-11-20 23:01:28 -0600

    moved readme to main directory fixed error causing the ship to turn when adjusting speed.

--- scripts/characters/player/player_controller.gd
+++ scripts/characters/player/player_controller.gd
@@ -15,7 +15,8 @@ var right_touch = false
 func _on_speed_slider_value_changed(value):
 	move_speed = max_move_speed * (value / 100.0)
 
-func _input(event):
+func _unhandled_input(event):
+	print("input unhandled")
 	if event is InputEventScreenTouch:
 		if event.pressed:
 			touch_positions[event.index] = event.position
